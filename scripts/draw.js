4f811ea143b634d65f841974e3c4e1073b7d9d9f
Jacob Knaack <jacobknaack@Jacobs-MacBook-Pro-2.local>
2016-02-20 10:15:03 -0800

    draws circle and lines currently

--- scripts/draw.js
+++ scripts/draw.js
@@ -1,10 +1,10 @@
-//testing the drawing function
+//creates our canvas where shapes can be made
 var elem = document.getElementById('artboard');
 var params = {width: 500, height: 500};
 var two = new Two(params).appendTo(elem);
 
 
-//creates a two Circle object
+//creates a two.circle object
 function drawCircle(x, y) {
   var circle = two.makeCircle(x, y, 50);
 
@@ -27,7 +27,7 @@ function renderCircle() {
   })
 }
 
-//creates a line object
+//creates a two.line object
 function drawLine(x, y, x2, y2) {
   var line = two.makeLine(x, y, x2, y2);
 
@@ -36,22 +36,43 @@ function drawLine(x, y, x2, y2) {
 }
 
 //places the two.line on the artboard
-function renderLine(){
-  $('#artboard').click(function(e) {
-    if (x == null){
+function renderLine() {
+  var x; var y; var x2; var y2;
+  $('#artboard')
+    .mousedown(function(e) {
+      var offset = $(this).offset();
+      x = (e.pageX - offset.left);
+      y = (e.pageY - offset.top);
+    })
+    .mouseup(function(e) {
+      var offset = $(this).offset();
+      x2 = (e.pageX - offset.left);
+      y2 = (e.pageY - offset.top);
+      drawLine(x, y, x2, y2);
+    })
+    console.log(two.scene);
+}
+
+//creates a two.rectangle object
+function drawRect(x, y, w, h) {
+  var rect = two.makeRectangle(x, y, w, h);
+
+  rectangle.stroke = '#000000';
+  rectangle.fill = '#ffffff';
+  two.update();
+}
+
+function renderRect(){
+  $('artboard')
+    .mousedown(function(e) {
       var offset = $(this).offset();
       var x = (e.pageX - offset.left);
-      var y = (e.pageY - offset.top);
-      console.log(x, y, x2, y2);
-      x2 = null;
-    } else if (x != null ) {
-      offset = $(this).offset();
-      var x2 = (e.pageX - offset.left);
-      var y2 = (e.pageY - offset.top);
-      drawLine(x, y, x2, y2);
-      x = null;
-    }
-      console.log(x, y, x2, y2);
-      console.log(two.scene);
-  })
+      var y = (e.pageY - offset.left);
+      console.log(x, y);
+    })
+    .mouseup(function(e) {
+      var offset = $(this).offset();
+      var w = (e.pageX);
+      var h = (e.pageY);
+    })
 }
