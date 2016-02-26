e1ce46724e3f317ecac4ffe1496be907201a7268
Cortney Lane <cdlane124@gmail.com>
2016-02-26 15:34:52 -0800

    server issue resolution

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,13 +1,15 @@
 // Upon clicking "Save to Gallery", the canvas image's URL will be grabbed,
-// and will post to my AWS S3 bucket, shapesnstuff, directory Artwork.
-// Also, the gallery page will display all images that are in the Artwork
-// directory of the shapesnstuff bucket.
+// and will post to the Artwork directory in my AWS S3 'shapesnstuff' bucket.
+// The gallery will then get and display the images from the Artwork directory.
+
 $(document).ready(function() {
 
   $('#galleryPost').click(function() {
     var canvas = document.getElementById("artboard");
-    var dataUrl = canvas.toDataURL("image/png").atob();
-    console.log(dataUrl);
-    });
+    var dataUrl = canvas.toDataURL("image/png");
+    $.post('/imgUpload', dataUrl, function(data, status, XHR) {
+      console.log(data);
+    })
+  });
 
 })
--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 var AWS = require('aws-sdk'); // Amazon Web Services storage
+
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 var s3 = new AWS.S3();
-
 var fs = require('fs');
-var colorImg = fs.readFileSync('./img/colors.png');
 
-/**
- * Don't hard-code your credentials!
- * Export the following environment variables instead:
- *
- * export AWS_ACCESS_KEY_ID='AKID'
- * export AWS_SECRET_ACCESS_KEY='SECRET'
- */
+var colorImg = fs.readFileSync('./img/colors.png');
+var bodyParser = require('body-parser');
+
 
 // Set your region for future requests.
 AWS.config.region = 'us-west-2';
 
+/*
 s3.upload({
   ACL: 'public-read',
   Bucket: 'shapesnstuff',
@@ -30,9 +26,28 @@ s3.upload({
     console.log('Image put!');
   }
 })
+*/
 
-var url = s3.getSignedUrl('getObject', {Bucket: 'shapesnstuff', Key: 'Artwork/colorImg.png', Expires: 95395738});
+var url = s3.getSignedUrl('getObject', {Bucket: 'shapesnstuff', Key: 'Artwork/colorImg.png', Expires: 953957380000});
 console.log(url);
+
+
+app.post('/imgUpload', bodyParser.text({extended: false,type: 'urlencoded'}), function(req, res) {
+  console.log(req.body);
+  s3.upload({
+    ACL: 'public-read',
+    Bucket: 'shapesnstuff',
+    Key: 'Artwork/colorImg4.png',
+    Body: new Buffer(req.body.replace(/^data:image\/(png|jpg);base64,/, ''), 'base64')
+  }, function(err, data) {
+    if (err) {
+      console.log(err);
+      console.log('Error uploading data: ' + data);
+    } else {
+      console.log('New image put!');
+    }
+  })
+})
 
 // s3.listObjects({Bucket: 'shapesnstuff', Prefix: 'Artwork/'}, function(err, data) {
 //     if (err) {
@@ -43,9 +58,9 @@ console.log(url);
 //     }
 // });
 
-
-
 // request.send();
+
+app.use(express.static(__dirname));
 
 app.listen(port, function() {
   console.log('Server started on port ' + port + '!')
