bf65ae34ecd81d0090193882c2a6b597634c1b97
Cortney Lane <cdlane124@gmail.com>
2017-01-12 07:41:10 -0800

    fix and clean up server code

--- server.js
+++ server.js
@@ -4,50 +4,39 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 var s3 = new AWS.S3();
-var fs = require('fs');
-
-var colorImg = fs.readFileSync('./img/colors.png');
 var bodyParser = require('body-parser');
 
-
-// Set your region for future requests.
 AWS.config.region = 'us-west-2';
 
 
 app.post('/imgUpload', bodyParser.text({extended: false,type: 'urlencoded'}), function(req, res) {
-  console.log(req.body);
 
   s3.upload({
     ACL: 'public-read',
     Bucket: 'shapesnstuff',
-    Key: 'Artwork/colorImg4.png',
+    Key: 'Artwork/image.png',
     Body: new Buffer(req.body.replace(/^data:image\/(png|jpg);base64,/, ''), 'base64')
   }, function(err, data) {
     if (err) {
-      console.log(err);
-      console.log('Error uploading data: ' + data);
+      throw new Error ('Error uploading data: ', err);
     } else {
-      console.log('New image put!');
+      res.send(data);
     }
-  })
-})
-
-app.get('/art', function(req, res) {
-  s3.listObjects({Bucket: 'shapesnstuff', Prefix: 'Artwork/'}, function(err, data) {
-      if (err) {
-        console.log(err);
-        console.log(data);
-      } else {
-        console.log(data);
-        res.send(data);
-      }
   });
 });
 
-// request.send();
+app.get('/art', function(req, res) {
+  s3.listObjects({Bucket: 'shapesnstuff', Prefix: 'Artwork/'}, function(err, data) {
+    if (err) {
+      throw new Error('Error obtaining images: ', err);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 app.use(express.static(__dirname));
 
 app.listen(port, function() {
-  console.log('Server started on port ' + port + '!')
-})
+  console.log('Server started on port ' + port + '!');
+});
